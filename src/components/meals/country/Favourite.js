4495e84381ce0d7a1861f3ddee87ffffe766b5c9
Adesola Oni-Shogbonyo <s.oni-shogbonyo@hotmail.co.uk>
2019-12-03 16:16:49 +0000

    working on a favourites functionality and error handling

--- src/components/meals/country/Favourite.js
+++ src/components/meals/country/Favourite.js
@@ -3,37 +3,73 @@ import axios from 'axios'
 import { Link } from 'react-router-dom'
 import Card from '../Card'
 import _ from 'lodash'
+import NoResultsHolder from '../indexPageTools/NoResultHolder'
 
 
 class FavouriteMeals extends React.Component {
   constructor() {
     super()
     this.state = {
-
-
+      names: [], 
+      allfavs: []
     }
    
   }
 
 
   componentDidMount() {
-
+    var names = JSON.parse(localStorage.getItem('names'))
+    names = names.map(el => +el)
+    this.setState({ names }, () => {
+      this.getInfoFromStorage()
+    })
+      
   }
 
+
+  getInfoFromStorage(){
+    if(this.state.names){
+      const retrieveFav = this.state.names.map(meal => meal)
+
+      retrieveFav.map(x =>
+        axios.get('https://www.themealdb.com/api/json/v1/1/lookup.php?i=' + x)
+          .then(res => this.setState({ allfavs: [...res.data.meals].concat(this.state.allfavs) })
+          ))
+
+
+    }
+  }
   
 
   render() {
-    if (!this.sta) return <h2>Loading...</h2>
+    console.log(this.state.allfavs)
+    if (this.state.allfavs.length === 0) return(
+      <NoResultsHolder />
+    )
     return (
       <section className="section">
         <div className="container">
           <hr />
         </div>
-        <div className="container">
-          <div className="columns">
+        <div className="column">
+          <div className="columns is-multiline">
+            {this.state.allfavs.map(meal =>
+              <div className="column is-half-tablet is-one-quarter-desktop"
+                key={meal.idMeal}
+              >
+                <Link to={`/meals/${meal.idMeal}`}
 
-            <h1>Heyy</h1>
-            
+                >
+                  <Card
+                    name={meal.strMeal}
+                    image={meal.strMealThumb}
+                    area={meal.strArea}
+
+                  />
+
+                </Link>
+              </div>
+            )}
           </div>
         </div>
         
